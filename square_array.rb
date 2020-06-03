6b7f1be19d7fb4565cc72e02814e650191421fa0
Jack Goode <jack.w.goode@icloud.com>
2020-06-03 00:53:37 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,11 @@
 def square_array(array)
+<<<<<<< HEAD
    new_array = []
    i = 0
    array.each{|num|  new_array[i] = num**2
    i += 1}
    new_array
+=======
+    array.each{|num|   num**2}
+>>>>>>> c3fdb7b5adb6f763f56ebcacb92d194f4a6752e9
 end
